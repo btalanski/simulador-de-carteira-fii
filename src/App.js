4267dc68c2b0988bf835d94f8324890a75a5bee1
Bruno Talanski <brn.talanski@gmail.com>
2020-10-21 18:49:20 -0300

    New AddFund component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import AddFund from './components/AddFund';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="fixed min-w-full flex p-3 bg-gray-900">
+        <h1 className="text-gray-100">Simulador de Carteira FII</h1>
       </header>
+
+      <main className="container m-auto pt-16">
+      <AddFund />
+      </main>
     </div>
   );
 }
