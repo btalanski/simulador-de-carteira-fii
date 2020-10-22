4d35933799968a583888b389cd389c3dc5936159
Bruno Talanski <brn.talanski@gmail.com>
2020-10-22 18:38:47 -0300

    Remove files

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="fixed min-w-full flex p-3 bg-gray-900 shadow-2xl">
-        <h1 className="text-gray-100"> Simulador de Carteira FII </h1>{' '}
+        <h1 className="text-gray-100"> Simulador de Carteira FII </h1>
       </header>
       <main className="container m-auto pt-16">
         <Portfolio />
-      </main>{' '}
+      </main>
     </div>
   );
 }
--- src/components/Portfolio/PortifolioContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const PortifolioContext = React.createContext(null);
-
-export default PortifolioContext;
